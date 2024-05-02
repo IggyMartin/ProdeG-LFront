bfcad8b255d3578a0c2cf921e0ae6c82c23fe23e
IggyMartin <iggymartin02102000@gmail.com>
2024-05-02 12:19:37 -0300

    Basic HomePage strcuture and Layout changes

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,51 @@
-import Layout from "./Layout"
+import fondoCopaAmerica2024 from '../assets/fondo-copa-america-2024/fondo_copa_america_01.jpeg'
+import GL from '../assets/logo-GyL/GL.png'
 
 function HomePage() {
   return (
-    <Layout>
-    </Layout>
+    <div className="bg-cover bg-center h-screen relative text-white" style={{backgroundImage: `url(${fondoCopaAmerica2024})`}}>
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <section className="flex justify-around items-center h-32 mb-4 opacity-95">
+        <img className="w-32 h-32" src={GL} alt="Logo GyL" />
+        <button className='bg-blue-800 text-[24px] px-6 rounded-2xl w-18 h-12'>Fixture</button>
+        <button className='bg-blue-800 text-[24px] px-6 rounded-2xl w-18 h-12'>Posiciones</button>
+        <button className='bg-blue-800 text-[24px] px-6 rounded-2xl w-18 h-12'>Reglas</button>
+        <button className='bg-blue-800 text-[24px] px-6 rounded-2xl w-18 h-12'>Premios</button>
+      </section>
+      <section className='opacity-95'>
+        <h2>Prediccion de tus 4 mejores de America</h2>
+        <div>
+          {/*include all selects (like the one below) in the div*/}
+          <label htmlFor="champion">Campeon</label>
+          <select name="champion" id="champion">
+            {/*do logic for every country as an option. Probably the select is a separated component*/}
+          </select>
+        </div>
+        <button>Guardar</button>
+      </section>
+      <section className='opacity-95'>
+        <table>
+          <thead>
+            <tr>
+              <th>Mi Posicion</th>
+              <th>Mi Puntaje</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{/*Logicfor user's position in table*/}</td>
+              <td>{/*Logic for user's points*/}</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+      <section className="flex justify-around items-center h-32 opacity-95">
+        <button className='bg-blue-800 text-[24px] px-6 rounded-2xl w-18 h-12'>Fase de grupos</button>
+        <button className='bg-blue-800 text-[24px] px-6 rounded-2xl w-18 h-12'>Cuartos de final</button>
+        <button className='bg-blue-800 text-[24px] px-6 rounded-2xl w-18 h-12'>Semifinal</button>
+        <button className='bg-blue-800 text-[24px] px-6 rounded-2xl w-18 h-12'>Final</button>
+      </section>
+    </div>
   )
 }
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,24 +1,23 @@
 import fondo08 from '../assets/fondos-de-pantalla/fondo08.jpg'
-import logo_copa_america from '../assets/logo-copa/logo_copa_america.png'
+import logo_copa_america from '../assets/logo-copa/logo_copa_america_2024.jpg'
 import GL from '../assets/logo-GyL/GL.png'
 
 function Layout({ children }) {
   return (
     <div className="bg-cover bg-center h-screen" style={{backgroundImage: `url(${fondo08})`}}>
-  {/* Background image with opacity */}
-  <div className="absolute inset-0 bg-stone-400 opacity-65"></div>
+        {/* Background image with opacity */}
+        <div className="absolute inset-0 bg-black opacity-65"></div>
 
-  {/* Content */}
-  <div className="relative flex flex-col justify-between p-4 text-black">
-    <div className="flex justify-between mb-4">
-      <img className="w-32 h-32" src={logo_copa_america} alt="Logo Copa America" />
-      <img className="w-32 h-32" src={GL} alt="Logo GyL" />
+        {/* Content */}
+        <div className="relative flex flex-col justify-between p-4 text-white">
+            <div className="flex justify-between mb-4">
+            <img className="w-32 h-32 rounded-2xl" src={GL} alt="Logo GyL" />
+            <img className="w-32 h-32 rounded-2xl" src={logo_copa_america} alt="Logo Copa America" />
+            </div>
+            {/* Additional content goes here */}
+            {children}
+        </div>
     </div>
-    {/* Additional content goes here */}
-    {children}
-  </div>
-</div>
-
   )
 }
 
