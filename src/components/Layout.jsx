041e8ad6ef98c6cc0b329184edf858e31e6c0728
IggyMartin <iggymartin02102000@gmail.com>
2024-05-03 13:18:42 -0300

    changes in structure and new route

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,19 +1,18 @@
-import fondo08 from '../assets/fondos-de-pantalla/fondo08.jpg'
 import logo_copa_america from '../assets/logo-copa/logo_copa_america_2024.jpg'
 import GL from '../assets/logo-GyL/GL.png'
 
-function Layout({ children }) {
+function Layout({ children, backgroundImage }) {
   return (
-    <div className="bg-cover bg-center h-screen" style={{backgroundImage: `url(${fondo08})`}}>
+    <div className="bg-cover bg-center h-screen" style={{backgroundImage: `url(${backgroundImage})`}}>
         {/* Background image with opacity */}
         <div className="absolute inset-0 bg-black opacity-65"></div>
 
         {/* Content */}
         <div className="relative flex flex-col justify-between p-4 text-white">
-            <div className="flex justify-between mb-4">
+            <header className="flex justify-between mb-4">
             <img className="w-32 h-32 rounded-2xl" src={GL} alt="Logo GyL" />
             <img className="w-32 h-32 rounded-2xl" src={logo_copa_america} alt="Logo Copa America" />
-            </div>
+            </header>
             {/* Additional content goes here */}
             {children}
         </div>
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../components/HomePage'
 import RulesAndConditions from '../components/RulesAndConditions'
+import Stage from '../components/Stage'
 
 function Router() {
   return (
     <Routes>
         <Route path='/home' element={<HomePage />} />
-        <Route path='rules&conditions' element={<RulesAndConditions />}/>
+        <Route path='/rules&conditions' element={<RulesAndConditions />}/>
+        <Route path='/groupStage' element={<Stage stage="Fase De Grupos"/>}/>
     </Routes>
   )
 }
