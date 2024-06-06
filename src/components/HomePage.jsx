fc4b359128702bc20d8e7b10407ee1f307072e48
PGochman <pedrogoch55@gmail.com>
2024-06-06 13:02:39 -0300

    Cambiar ruta erronea y agregar etiquetas a predicciones

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -109,7 +109,7 @@ function HomePage() {
               <div className="w-52">
                 <ReactSelect
                 className="text-black bg-blue-700"
-                options={[{value: "groupStage", label: "Fase de grupos"}, {value: "quarterfinals", label: "Cuartos de final"}, {value: "semifinals", label: "Semifinales"}, {value:"finals", label: "Estancia final"}]}
+                options={[{value: "groups", label: "Fase de grupos"}, {value: "quarterfinals", label: "Cuartos de final"}, {value: "semifinals", label: "Semifinales"}, {value:"finals", label: "Estancia final"}]}
                 placeholder="FIXTURE"
                 isSearchable={false}
                 onChange={(chosenStage) => navigate(`/${chosenStage.value}`)}
@@ -123,12 +123,15 @@ function HomePage() {
             globalUser?.topFourCountriesPrediction.length > 0 ? (
                 <section className="w-1/2 flex justify-between">
                   {
-                    topFourPredictionCountriesWithFlags(globalUser.topFourCountriesPrediction).map(country => (
-                      <div className="flex items-center gap-2">
-                        <img className="w-10 h-10 object-cover rounded-full" src={country.flag} alt="country image" />
-                        <span>{country.name}</span>
+                    topFourPredictionCountriesWithFlags(globalUser.topFourCountriesPrediction).map((country, index) => (
+                      <div className="flex flex-col items-center gap-1 border border-slate-200 rounded-[16px] px-4 py-1">
+                        <h1>{index == 0 ? "Campeón" : index == 1 ? "Subcampeón" : index==2 ? "Tercer puesto" : "Cuarto puesto"}</h1>
+                        <div className="flex items-center gap-2">
+                          <img className="w-10 h-10 object-cover rounded-full" src={country.flag} alt="country image" />
+                          <span>{country.name}</span>
+                        </div>
                       </div>
-                    ))
+                      ))
                   }
                 </section>
             ) : (
