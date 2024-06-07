9dc89df2771e3fab7ac1840dce065318db95c9e2
PGochman <pedrogoch55@gmail.com>
2024-06-07 18:35:45 -0300

    Agregar metodo para obtener resultados de top 4

--- src/services/topFourPredictionService.js
+++ src/services/topFourPredictionService.js
@@ -1,4 +1,4 @@
-import { postWithHeaders } from "./config";
+import { getWithHeaders, postWithHeaders } from "./config";
 
 const PlayerPATH = "/prode/topFourCountriesPrediction"
 const AdminPATH = "/prode/topFourCountriesRanking"
@@ -17,6 +17,16 @@ export const createAdminTopFourPredictionDB = async (data) => {
     const url = `${AdminPATH}/create`
     try {
         const response = await postWithHeaders(url, data)
+        return response.data
+    } catch (error) {
+        throw error.response
+    }
+}
+
+export const getTopFourRanking = async () => {
+    const url = `${AdminPATH}/all`
+    try {
+        const response = await getWithHeaders(url)
         return response.data
     } catch (error) {
         throw error.response
