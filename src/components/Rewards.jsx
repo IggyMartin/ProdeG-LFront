d4ceb7ec8aed2e5aecaa48428ad6514a9e6bba29
PGochman <pedrogoch55@gmail.com>
2024-06-07 10:50:16 -0300

    Quitar titulo innecesario de premios

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -4,7 +4,6 @@ import premios from '../assets/fondos-de-pantalla/rewards.png'
 function Rewards() {
   return (
    <Layout page={null}>
-    <h2 className="pl-8 text-[36px]">Premios</h2>
     <img className="w-2/5 h-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" src={premios} alt="premios" />
    </Layout>
   )
