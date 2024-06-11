f6f3f37de61b46ceee03214a08a314897cead4ed
Ignacio Martin <iggymartin02102000@gmail.com>
2024-06-11 15:30:07 -0300

    agrego estrellas a seccion de premios

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -1,10 +1,14 @@
 import Layout from "./Layout";
 import premios from '../assets/fondos-de-pantalla/rewards.png'
+import estrellas from '../assets/fondos-de-pantalla/estrellas.png'
 
 function Rewards() {
   return (
    <Layout page={null}>
-    <img className="w-2/5 h-auto absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" src={premios} alt="premios" />
+    <div className="w-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <img className="mx-auto pl-6 w-[5%] h-aut0 relative top-4" src={estrellas} alt="estrellas" />
+      <img className="w-2/5 h-auto mx-auto" src={premios} alt="premios" />
+    </div>
    </Layout>
   )
 }
